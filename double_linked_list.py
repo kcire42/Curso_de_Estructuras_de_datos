68a9778ec3e69c2037caf92e33a5607279a53a41
Erick Carrillo <erickcarrillo2807@gmail.com>
2022-09-23 19:36:35 -0500

    double linked list

--- double_linked_list.py
+++ double_linked_list.py
@@ -1,12 +1,18 @@
 from crear_node import Node
 
+class Node(object):
+    def __init__(self, data , next = None):
+        self.data = data
+        self.next = next
+
+
 class TwoWayNode(Node):
 
-    def __init__(self, data , next=None , previous=None ) -> None:
-        super().__init__(data, next)
+    def __init__(self, data , previous = None ,  next=None ):
+        Node.__init__(self,data,next)
         self.previous = previous
 
-  
+    
 
 
 if __name__ == '__main__':
