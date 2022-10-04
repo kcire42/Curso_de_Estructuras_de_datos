55a075861e914f1ee761e5ed5759072b41ef4ddd
Erick Carrillo <erickcarrillo2807@gmail.com>
2022-10-04 12:21:04 -0500

    inserta nodo en linked list

--- linked_list.py
+++ linked_list.py
@@ -3,10 +3,20 @@ from node import Node
 
 
 class Linked_List:
-    def __init__(self) -> None:
+    def __init__(self,nodes = None) -> None:
         self.head = None
+        if nodes is not None:
+            node = Node(data = nodes.pop(0))
+            self.head = node
+            for element in nodes:
+                node.next = Node(data=element)
+                node = node.next
 
+
+    #FUNCION PARA EL RETORNO DE STR 
+    
     def __str__(self) -> str:
+        """REGRESA EL CONTENIDO DE MI LINKED LIST EN STR Y LA POSICION DE COMO VAN ORDENADAS"""
         node = self.head
         nodes = []
         while node is not None:
@@ -14,18 +24,51 @@ class Linked_List:
             node = node.next
         nodes.append("None")
         return "-> ".join(nodes)
+    
+
+    #FUNCION PARA HACER ITERACION DEL ITER
+    def __iter__(self):
+        """FUNCION QUE REGRESA MI LINKED LIST ITERANDO LOS ELEMENTOS"""
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __insert_first_pos__(self,new_value):
+        node = Node(new_value)
+        node.next = self.head
+        self.head = node 
+        
+    def __insert_last_pos__(self,new_value):
+        if self.head is None:
+            self.head = new_value
+            return
+        for current_node in self:
+            pass
+        current_node.next = node
+        
+        
 
 
 
 if __name__ == '__main__':
     llist = Linked_List()
-    print(llist.__str__())
+    print(llist)
     first = Node("a")
     llist.head = first
-    print(llist.__str__())
+    print(llist)
     second = Node("b")
     first.next = second
-    print(llist.__str__())
+    print(llist)
     third = Node("c")
     second.next = third
-    print(llist.__str__())
+    print(llist)
+    llist_1 = Linked_List(["A","B","C","D","E"])
+    print(llist_1)
+    for node in llist_1:
+        print(node)
+    print(llist_1)
+    llist_1.__insert_first_pos__("F")
+    print(llist_1)
+    llist_1.__insert_last_pos__("F")
+    print(llist_1)
--- node.py
+++ node.py
@@ -2,7 +2,7 @@
 #DEFINIMOS NUESTRA CLASE NODO 
 
 class Node:
-
+    """CREACION DE NODO REQUIERE UN PARAMETRO DE DATO Y CUANDO SE CREA EL NODO APUENTA A NONE"""
     def __init__(self,data) -> None:
         self.data = data
         self.next = None
